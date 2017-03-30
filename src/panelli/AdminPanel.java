a7c4d0363875144b9cd7dc458d9bf8b200ddaf71
SirSimonSays <simone@simone>
2017-03-30 16:52:39 +0200

    try to change layout

--- src/panelli/AdminPanel.java
+++ src/panelli/AdminPanel.java
@@ -1,5 +1,11 @@
 package panelli;
 
 public class AdminPanel extends DefaultPanel{
+	
+	/**
+	 * @var TAG
+	 * Tag univoco utilizzato per identificare questa schermata
+	 */
+	public static final String TAG = "admin";
 
 }
